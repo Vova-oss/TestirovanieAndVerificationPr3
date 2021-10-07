78114111df3378e93257492465afc5855a0d103c
My Name <ogyrcka@gmail.com>
2021-10-08 02:10:46 +0300

    Test for createArms()

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.LinkedList;
+
 @SpringBootTest
 class DemoApplicationTests {
 
@@ -11,6 +13,16 @@ class DemoApplicationTests {
         assert Main.cards.isEmpty();
         Main.createDeck();
         assert Main.cards.size() == 36;
+    }
+
+    @Test
+    void testCreateArms() {
+        Main.createDeck();
+        Main.firstPlayer = new LinkedList<>();
+        Main.secondPlayer = new LinkedList<>();
+        Main.createArms();
+        assert Main.firstPlayer.size() == 6;
+        assert Main.secondPlayer.size() == 6;
     }
 
 }
